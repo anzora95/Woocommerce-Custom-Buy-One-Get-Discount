5470427daf7fbeaf3ca4557709106fd5e28adae5
Jose <jose.anzora@vertikalcompany.com>
2021-04-12 10:51:21 -0600

    second version
    Added notice to alert on cart

--- base_april_customized.php
+++ base_april_customized.php
@@ -1,6 +1,10 @@
 <?php
+//customized BOGD
+
 add_action('woocommerce_cart_calculate_fees', 'add_custom_discount_2nd_at_50', 10, 1 );
 function add_custom_discount_2nd_at_50( $wc_cart ){
+
+
     if ( is_admin() && ! defined( 'DOING_AJAX' ) ) return;
     $discount = 0;
     $items_prices = array();
@@ -8,25 +12,45 @@ function add_custom_discount_2nd_at_50( $wc_cart ){
 
     // Set HERE your targeted variable product ID
     $targeted_product_id = "834"; //producto targeted
+    // Loop through all products in the Cart
+foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 
-    foreach ( $wc_cart->get_cart() as $key => $cart_item ) {                      //for each de cada producto
-        //if( $cart_item['product_id'] == $targeted_product_id ){                   //cada uno de los elementos del carro evaluados si el id es igual al del targeted
-            $qty = intval( $cart_item['quantity'] );                              //transforma la cantidad de elementos en el carro en enteros
-            for( $i = 0; $i < $qty; $i++ )                                        //recorre los elementos en el carro
-                $items_prices[] = floatval( $cart_item['data']->get_price());     //agrega cada uno de los precios del carro a un arreglo
-        //}
+    // If Cart has category "accesorios", set $cat_in_cart to true
+    if ( has_term( 'accesorios', 'product_cat', $cart_item['product_id'] ) ) {
+        $cat_in_cart = true;
+        break;
     }
-    $count_items_prices = count($items_prices);                                   //cuenta cuantos elementos hay en el arreglo de precios
-    if( $count_items_prices > 1 ){                                                 // si hay algun elemento en el arreglo (osea > 1)
-      foreach( $items_prices as $key => $price ) {                                   // $items_prices en el for each
-          $a++;                                                                     //contador para cada itereacion
-          if ($a == 2) {                                                            // evalua si es la segunda iteracion
-            $discount -= number_format($price / 2, 2 );                             //si es la segunda itereacion que divida el precio entre 2 y que le de formato de dos decimales
-            break;                                                                  // que haga el break
-          }
-        }
+    else {
+      //start 50%
+         foreach ( $wc_cart->get_cart() as $key => $cart_item ) {                      //for each de cada producto
+             //if( $cart_item['product_id'] == $targeted_product_id ){                   //cada uno de los elementos del carro evaluados si el id es igual al del targeted
+                 $qty = intval( $cart_item['quantity'] );                              //transforma la cantidad de elementos en el carro en enteros
+                 for( $i = 0; $i < $qty; $i++ )                                        //recorre los elementos en el carro
+                     $items_prices[] = floatval( $cart_item['data']->get_price());     //agrega cada uno de los precios del carro a un arreglo
+             //}
+         }
+         $count_items_prices = count($items_prices);                                   //cuenta cuantos elementos hay en el arreglo de precios
+         if( $count_items_prices > 1 ){                                                 // si hay algun elemento en el arreglo (osea > 1)
+           foreach( $items_prices as $key => $price ) {                                   // $items_prices en el for each
+               $a++;                                                                     //contador para cada itereacion
+               if ($a == 2) {                                                            // evalua si es la segunda iteracion
+                 $discount -= number_format($price / 2, 2 );                             //si es la segunda itereacion que divida el precio entre 2 y que le de formato de dos decimales
+                 break;                                                                  // que haga el break
+               }
+             }
+         }
+         //section ended
     }
+}
 
+if ( $cat_in_cart ) {
+
+// For example, print a notice
+wc_print_notice( 'Category Downloads is in the Cart!', 'notice' );
+
+// Or maybe run your own function...
+// ..........
+}
 
 
     if( $discount != 0 ){
